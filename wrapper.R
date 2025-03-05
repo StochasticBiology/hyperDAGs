c1267470dbc941870fc7980289c5eb231bd32ba8
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2025-03-05 09:20:19 +0100

    Update to plot styling and outputs

--- wrapper.R
+++ wrapper.R
@@ -97,6 +97,7 @@ for(expt in c("inline", "TBsimp", "TB", "CGH", "cancer", "mtDNA", "ptDNA")) {
   states = unique(c(ancnames, descnames))
   dset.states = data.frame(States=states)
   
+  # for the simplified TB experiment, output data in table graphics
   if(expt == "TBsimp") {
     df = 3
     tb.simp.data =  ggarrange(ggtexttable(dset.states, rows=NULL),
@@ -110,9 +111,13 @@ for(expt in c("inline", "TBsimp", "TB", "CGH", "cancer", "mtDNA", "ptDNA")) {
     dev.off()
   }
   
+  # *** CENTRAL LINE ***: apply the algorithm to find our solution
   s.dag = simplest.DAG(ancnames, descnames)
+  
+  # output some properties of the solution
   write.table(apply(as_edgelist(s.dag$best.graph), c(1,2), BinToDec), paste0(expt, "-table.txt", collapse=""), row.names=FALSE, col.names=FALSE, quote=FALSE)
   write.single.steps(apply(as_edgelist(s.dag$best.graph), c(1,2), BinToDec), L, paste0(expt, "-table-ss.txt", collapse=""))
+  # verify that our solution works
   if(length(descnames) != 0) {
     spanned = transitions.spanned(s.dag$best.graph, ancnames, descnames)
     if(spanned == TRUE) {
@@ -122,6 +127,7 @@ for(expt in c("inline", "TBsimp", "TB", "CGH", "cancer", "mtDNA", "ptDNA")) {
     }
   }
   
+  # output visualisations of the solution and steps along the way
   sf = 3
   png(paste0("stage-1-", expt, ".png", collapse=""), width=600*sf, height=300*sf, res=72*sf)
   print(plot.stage.1(s.dag))
@@ -130,6 +136,7 @@ for(expt in c("inline", "TBsimp", "TB", "CGH", "cancer", "mtDNA", "ptDNA")) {
   print(plot.stage.2(s.dag))
   dev.off()
   
+  # compare Python algorithm if desired
   if(run.python) {
   message("Python code:")
   write.table(data.frame(anc=ancnames,desc=descnames), paste0(expt, "-trans-basic.txt", collapse=""), quote = FALSE, sep=" ", row.names=FALSE, col.names=FALSE)
@@ -152,6 +159,9 @@ for(expt in c("inline", "TBsimp", "TB", "CGH", "cancer", "mtDNA", "ptDNA")) {
   expt.out[[expt.index]] = s.dag
 }
 
+### the following section produces graphical output for the article
+
+# some options for graphical styling in output
 scale.x = scale_x_continuous(expand = expansion(mult = c(0.05, 0.05)))
 scale.y = scale_y_continuous(expand = expansion(mult = c(0, 0.1))) 
 title.style = theme(plot.title = element_text(
@@ -162,6 +172,7 @@ title.style = theme(plot.title = element_text(
   vjust = 0 #1.5    # Adjust vertical alignment
 )) 
 
+# simplified TB plot
 png("fig-tbsimp.png", width=850*sf, height=700*sf, res=72*sf)
 print(ggarrange(
   tb.simp.data, 
@@ -174,6 +185,7 @@ print(ggarrange(
 )
 dev.off()
 
+# full TB plot
 png("fig-tb.png", width=1200*sf, height=500*sf, res=72*sf)
 w = plot.weights(expt.out[[3]]$best.graph, 
                  labels = c("INH", "RIF", "PZA", "EMB", "STR", "AMI", "CAP", 
@@ -183,6 +195,7 @@ print(ggarrange(plot.stage.p(expt.out[[3]]$best.graph) + scale.y + title.style,
                 labels=c("C", "D")))
 dev.off()
 
+# cancer progression plot. to match previous research we rotate the visualisations here
 png("fig-cancer.png", width=1000*sf, height=500*sf, res=72*sf)
 w = plot.weights(expt.out[[5]]$best.graph, 
                  labels = c("FLT3", "NPM1", "WT1", "DNMT3A", "KRAS", "NRAS", "RUNX1",
@@ -197,7 +210,9 @@ print(ggarrange(                 plot.stage.p(expt.out[[5]]$best.graph) + coord_
 )
 dev.off()
 
+# organelle DNA plot
 
+# highlight some species of interest
 mt.orgs = c("homo sapiens", "arabidopsis thaliana", "reclinomonas americana", 
          "saccharomyces cerevisiae", "plasmodium falciparum", "physcomitrium patens",
          "triticum aestivum")
@@ -208,8 +223,8 @@ mt.labs = data.frame()
 for(i in 1:length(mt.orgs)) {
   mt.labs = rbind(mt.labs, data.frame(Species = mt.orgs[i], label=mt.names[which(mt.raw$Species == mt.orgs[i])]))
 }
-#plot.stage.p(expt.out[[6]]$best.graph, v.labels=mt.labs)
 
+# same for plastids
 pt.orgs = c("arabidopsis thaliana", "physcomitrium patens", "hydnora visseri", "parasitaxus usta",
             "polysiphonia elongata", "gracilaria changii", "cladosiphon okamuranus")
 pt.raw = read.csv("pt-barcodes-manual.csv")
@@ -219,8 +234,8 @@ pt.labs = data.frame()
 for(i in 1:length(pt.orgs)) {
   pt.labs = rbind(pt.labs, data.frame(Species = pt.orgs[i], label=pt.names[which(pt.raw$Species == pt.orgs[i])]))
 }
-#plot.stage.p(expt.out[[7]]$best.graph, v.labels=pt.labs)
 
+# plot the solution, with labelled species highlights
 png("fig-odna.png", width=1000*sf, height=1000*sf, res=72*sf)
 print(ggarrange(plot.stage.p(expt.out[[6]]$best.graph, v.labels=mt.labs) + title.style, 
                 plot.stage.p(expt.out[[7]]$best.graph, v.labels=pt.labs) + title.style, 
@@ -228,11 +243,15 @@ print(ggarrange(plot.stage.p(expt.out[[6]]$best.graph, v.labels=mt.labs) + title
                 labels=c("A", "B")))
 dev.off()
 
+# additional organelle DNA and cancer plots, reflecting the orderings of individual features
+
 oDNA.types = c("MT", "PT", "AML")
 oDNA.g = list()
 oDNA.g2 = list()
 
+# loop through these case studies
 for(oDNA.expt in 1:3) {
+  # pull the solution for this case study
   oDNA = oDNA.types[oDNA.expt]
   if(oDNA == "MT") {
     wg = expt.out[[6]]$best.graph
@@ -241,6 +260,7 @@ for(oDNA.expt in 1:3) {
   } else if(oDNA == "AML") {
     wg = expt.out[[5]]$best.graph
   }
+  # get the node names in this solution
   these.nodes = ends(wg, E(wg)[1])
   src = strsplit(these.nodes[1,1], "")[[1]]
   L = length(src)
@@ -256,8 +276,10 @@ for(oDNA.expt in 1:3) {
                        "MPL", "SMC3")
   }
   
+  # populate a matrix that will record features present when each feature is gained, and vice versa
   mat.dep = matrix(0, nrow=L, ncol=L)
   
+  # go through the edges in the solution, recording how many features are present when each feature is gained
   for(i in 1:length(E(wg))) {
     these.nodes = ends(wg, E(wg)[i])
     src = strsplit(these.nodes[1,1], "")[[1]]
@@ -268,10 +290,12 @@ for(oDNA.expt in 1:3) {
     mat.dep[presents, absents] = mat.dep[presents, absents]+1
   }
   
+  # label this matrix and summarise in a dataframe for plotting
   colnames(mat.dep) = genes
   rownames(mat.dep) = genes
   gene.stats = data.frame(gene=genes, cols=colSums(mat.dep), rows=rowSums(mat.dep))
   
+  # produce the corresponding plots
   oDNA.g[[oDNA.expt]] = ggplot(gene.stats, aes(x=cols, y=rows, label=gene)) + 
     geom_point() + geom_text_repel(max.overlaps = 30) + scale_x_sqrt() + scale_y_sqrt() + 
     labs(x="Other losses preceding loss", y="Other losses following loss")
@@ -286,6 +310,7 @@ ggarrange(oDNA.g[[1]] + theme_minimal(), oDNA.g[[2]] + theme_minimal(),
           labels=c("A", "B"))
 dev.off()
 
+# output these plots
 png("oDNA-heatmaps.png", width=800*sf, height=1200*sf, res=72*sf)
 ggarrange(oDNA.g2[[1]] + theme_void(), oDNA.g2[[2]] + theme_void(), 
           labels=c("A", "B"), nrow=2, heights=c(1, 2.5))
