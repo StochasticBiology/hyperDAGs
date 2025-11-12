eb6b3950fbab43d10f7a18ac45d621de8f1401ec
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2025-11-12 12:42:36 +0100

    Demo function

--- R/simulation-functions.R
+++ R/simulation-functions.R
@@ -1,3 +1,19 @@
+#' Simple demonstration of HyperDAGs
+#'
+#' @examples
+#' hyperDAG_demo()
+#' @export
+hyperDAG_demo <- function() {
+  # simple illustrative dataset
+  ancnames  = c("10000","01000","00000","10010","01011","10110")
+  descnames = c("11110","11110","01011","11110","01111","11111")
+  # get STSD
+  s.dag = simplest_DAG(ancnames, descnames)
+  # plot and summarise: A is SSA, B is STSD, numbers are branching counts
+  plot_stage_2(s.dag)
+  fit_properties(s.dag)
+  transitions_spanned(s.dag$best.graph, ancnames, descnames)
+}
 
 #' Produce the set of binary strings of length nstr with k 1's
 #'
