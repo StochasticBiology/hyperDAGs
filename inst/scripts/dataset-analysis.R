751421b5bca78d9b69359115fed31a2ac5624fe5
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2025-11-12 12:09:47 +0100

    Option to pull in pre-run data

--- inst/scripts/dataset-analysis.R
+++ inst/scripts/dataset-analysis.R
@@ -31,55 +31,59 @@ sf = 3
 expt.index = 0
 expt.out = list()
 
-# mtDNA case study takes some minutes; ptDNA will take more
-for(expt in c("inline", "TBsimp", "TB", "CGH", "cancer", "mtDNA", "ptDNA")) {
-  if(expt == "mtDNA") { dset = get_mtDNA_data() }
-  if(expt == "ptDNA") { dset = get_ptDNA_data() }
-  if(expt == "cancer") { dset = get_cancer_data() }
-  if(expt == "inline") { dset = get_inline_data() }
-  if(expt == "TB") { dset = get_TB_data() }
-  if(expt == "TBsimp") { dset = get_TB_data(simple=TRUE) }
-  if(expt == "CGH") { dset = get_CGH_data() }
+run.sims = TRUE
 
-  L = dset[["L"]]
-  ancnames = dset[["ancnames"]]
-  descnames = dset[["descnames"]]
+if(run.sims == TRUE) {
+  # mtDNA case study takes some minutes; ptDNA will take more
+  for(expt in c("inline", "TBsimp", "TB", "CGH", "cancer", "mtDNA", "ptDNA")) {
+    if(expt == "mtDNA") { dset = get_mtDNA_data() }
+    if(expt == "ptDNA") { dset = get_ptDNA_data() }
+    if(expt == "cancer") { dset = get_cancer_data() }
+    if(expt == "inline") { dset = get_inline_data() }
+    if(expt == "TB") { dset = get_TB_data() }
+    if(expt == "TBsimp") { dset = get_TB_data(simple=TRUE) }
+    if(expt == "CGH") { dset = get_CGH_data() }
 
-  dset.trans = unique(data.frame(Ancestor=ancnames, Descendant=descnames))
-  states = unique(c(ancnames, descnames))
-  dset.states = data.frame(States=states)
+    L = dset[["L"]]
+    ancnames = dset[["ancnames"]]
+    descnames = dset[["descnames"]]
 
-  # for the simplified TB experiment, store data for plotting later
-  if(expt == "TBsimp") { tbsimp.dset.states = dset.states; tbsimp.dset.trans = dset.trans }
+    dset.trans = unique(data.frame(Ancestor=ancnames, Descendant=descnames))
+    states = unique(c(ancnames, descnames))
+    dset.states = data.frame(States=states)
 
-  # *** CENTRAL LINE ***: apply the algorithm to find our solution
-  s.dag = simplest_DAG(ancnames, descnames)
+    # for the simplified TB experiment, store data for plotting later
+    if(expt == "TBsimp") { tbsimp.dset.states = dset.states; tbsimp.dset.trans = dset.trans }
 
-  # verify that our solution works
-  if(length(descnames) != 0) {
-    spanned = transitions_spanned(s.dag$best.graph, ancnames, descnames)
-    if(spanned == TRUE) {
-      message("-- transitions verified")
-    } else {
-      message("-- transitions *NOT* verified")
+    # *** CENTRAL LINE ***: apply the algorithm to find our solution
+    s.dag = simplest_DAG(ancnames, descnames)
+
+    # verify that our solution works
+    if(length(descnames) != 0) {
+      spanned = transitions_spanned(s.dag$best.graph, ancnames, descnames)
+      if(spanned == TRUE) {
+        message("-- transitions verified")
+      } else {
+        message("-- transitions *NOT* verified")
+      }
     }
+
+    # output visualisations of the solution and steps along the way
+    sf = 3
+    png(paste0("stage-1-", expt, ".png", collapse=""), width=600*sf, height=300*sf, res=72*sf)
+    print(plot_stage_1(s.dag))
+    dev.off()
+    png(paste0("stage-2-", expt, ".png", collapse=""), width=600*sf, height=300*sf, res=72*sf)
+    print(plot_stage_2(s.dag))
+    dev.off()
+
+    expt.out[[expt]] = s.dag
   }
-
-  # output visualisations of the solution and steps along the way
-  sf = 3
-  png(paste0("stage-1-", expt, ".png", collapse=""), width=600*sf, height=300*sf, res=72*sf)
-  print(plot_stage_1(s.dag))
-  dev.off()
-  png(paste0("stage-2-", expt, ".png", collapse=""), width=600*sf, height=300*sf, res=72*sf)
-  print(plot_stage_2(s.dag))
-  dev.off()
-
-  expt.out[[expt]] = s.dag
+  save(expt.out, file="expt-out.Rdata")
+} else {
+  expt.out.file = system.file("extdata", "expt-out.Rdata", package="hyperdags")
+  load(expt.out.file)
 }
-
-#save(expt.out, file="expt-out.Rdata")
-expt.out.file = system.file("extdata", "expt-out.Rdata", package="hyperdags")
-load(expt.out.file)
 
 ### the following section produces graphical output for the article
 
@@ -95,25 +99,25 @@ title.style = theme(plot.title = element_text(
 ))
 
 
-  tb.simp.data =  ggarrange(ggtexttable(tbsimp.dset.states, rows=NULL),
-                            ggarrange(ggtexttable(tbsimp.dset.trans[1:(nrow(tbsimp.dset.trans)/3),], rows=NULL),
-                                      ggtexttable(tbsimp.dset.trans[(nrow(tbsimp.dset.trans)/3+1):(2*nrow(tbsimp.dset.trans)/3),], rows=NULL),
-                                      ggtexttable(tbsimp.dset.trans[(2*nrow(tbsimp.dset.trans)/3+1):(nrow(tbsimp.dset.trans)),], rows=NULL),
-                                      nrow=1),
-                            nrow = 1, labels=c("A", "B"), widths=c(0.3,1))
-  png("table-tbsimp.png", width=600*sf, height=480*sf, res=72*sf)
-  print(tb.simp.data)
-  dev.off()
+tb.simp.data =  ggarrange(ggtexttable(tbsimp.dset.states, rows=NULL),
+                          ggarrange(ggtexttable(tbsimp.dset.trans[1:(nrow(tbsimp.dset.trans)/3),], rows=NULL),
+                                    ggtexttable(tbsimp.dset.trans[(nrow(tbsimp.dset.trans)/3+1):(2*nrow(tbsimp.dset.trans)/3),], rows=NULL),
+                                    ggtexttable(tbsimp.dset.trans[(2*nrow(tbsimp.dset.trans)/3+1):(nrow(tbsimp.dset.trans)),], rows=NULL),
+                                    nrow=1),
+                          nrow = 1, labels=c("A", "B"), widths=c(0.3,1))
+png("table-tbsimp.png", width=600*sf, height=480*sf, res=72*sf)
+print(tb.simp.data)
+dev.off()
 
 # simplified TB plot
 png("fig-tbsimp.png", width=850*sf, height=700*sf, res=72*sf)
 print(ggarrange(
   tb.simp.data,
   ggarrange(plot_stage_p(expt.out[[2]]$raw.graph) + scale.y + title.style,
-                plot_stage_p(expt.out[[2]]$rewired.graph) + scale.y + title.style,
-                plot_stage_p(expt.out[[2]]$best.graph) + scale.y + title.style,
-                nrow = 1,
-                labels=c("C", "D", "E")),
+            plot_stage_p(expt.out[[2]]$rewired.graph) + scale.y + title.style,
+            plot_stage_p(expt.out[[2]]$best.graph) + scale.y + title.style,
+            nrow = 1,
+            labels=c("C", "D", "E")),
   nrow=2, heights=c(1,0.66))
 )
 dev.off()
@@ -147,13 +151,13 @@ library(stringr)
 load("~/Dropbox/Documents/2024_Projects/HyperEvol/Perspective/EvAM-MDR-main/post-inference.Rdata")
 old.tb.data = plotHypercube.curated.tree(src.data,font.size = 3)
 old.tb.plot = plotHypercube.sampledgraph2(fitted.model, edge.label.size=4, truncate = 6,
-                            edge.label.angle = "along", node.labels=FALSE,
-                            no.times=TRUE) + theme(legend.position = "none") +
+                                          edge.label.angle = "along", node.labels=FALSE,
+                                          no.times=TRUE) + theme(legend.position = "none") +
   theme(plot.margin = unit(c(0, 0., 0, 0.), "cm"))
 
 
 oct.plot.tb = ggarrange(old.tb.data, w$thresh.plot, old.tb.plot,
-          nrow=1, widths=c(0.75,1,1), labels=c("Ai", "ii", "iii"))
+                        nrow=1, widths=c(0.75,1,1), labels=c("Ai", "ii", "iii"))
 
 png("oct-plot-tb.png", width=800*sf, height=300*sf, res=72*sf)
 print(oct.plot.tb)
@@ -185,19 +189,19 @@ print(ggarrange(                 plot_stage_p(expt.out[["cancer"]]$best.graph) +
 dev.off()
 
 w.cancer = plot_weights(expt.out[["cancer"]]$best.graph,check.overlaps = TRUE,
-                 labels = c("FLT3", "NPM1", "WT1", "DNMT3A", "KRAS", "NRAS", "RUNX1",
-                            "IDH1", "IDH2", "PTPN11", "SRSF2", "ASXL1", "BCOR", "STAG2",
-                            "TP53", "U2AF1", "SF3B1", "TET2", "CSF3R", "JAK2", "GATA2",
-                            "EZH2", "PPM1D", "SETBP1", "KIT", "CBL", "PHF6", "MYC", "ETV6",
-                            "MPL", "SMC3"), orient="along", edge.label.size = 4)
+                        labels = c("FLT3", "NPM1", "WT1", "DNMT3A", "KRAS", "NRAS", "RUNX1",
+                                   "IDH1", "IDH2", "PTPN11", "SRSF2", "ASXL1", "BCOR", "STAG2",
+                                   "TP53", "U2AF1", "SF3B1", "TET2", "CSF3R", "JAK2", "GATA2",
+                                   "EZH2", "PPM1D", "SETBP1", "KIT", "CBL", "PHF6", "MYC", "ETV6",
+                                   "MPL", "SMC3"), orient="along", edge.label.size = 4)
 
 png("fig-cancer-new.png", width=800*sf, height=500*sf, res=72*sf)
 ggarrange(oct.plot.tb,
           w.cancer$thresh.plot +
-      #  scale_y_reverse() +
-      #  coord_flip()+
-        theme(plot.margin = margin(t = 1, r = 1, b = 1, l = 1)) ,
-        nrow=2, heights=c(1,0.75), labels=c("", "B"))
+            #  scale_y_reverse() +
+            #  coord_flip()+
+            theme(plot.margin = margin(t = 1, r = 1, b = 1, l = 1)) ,
+          nrow=2, heights=c(1,0.75), labels=c("", "B"))
 dev.off()
 
 load("~/Dropbox/Documents/2024_Projects/HyperEvol/Workspaces/cancer-mar-8.RData")
@@ -210,9 +214,9 @@ g.cancer.graph2 = plotHypercube.sampledgraph2(parallelised.runs[[4]], use.arc = 
 
 png("fig-cancer-comp-new.png", width=1000*sf, height=500*sf, res=72*sf)
 ggarrange(w.cancer$thresh.plot +
-              scale_y_reverse() +
-              coord_flip(),
-            g.cancer.graph2 +  coord_flip() + scale_y_reverse(),
+            scale_y_reverse() +
+            coord_flip(),
+          g.cancer.graph2 +  coord_flip() + scale_y_reverse(),
           nrow=1)
 dev.off()
 
@@ -234,8 +238,8 @@ transform_name = function(x) {
 }
 # highlight some species of interest
 mt.orgs = c("homo sapiens", "arabidopsis thaliana", "reclinomonas americana",
-         "saccharomyces cerevisiae", "plasmodium falciparum", "physcomitrium patens",
-         "triticum aestivum")
+            "saccharomyces cerevisiae", "plasmodium falciparum", "physcomitrium patens",
+            "triticum aestivum")
 datafile <- system.file("extdata", "mt-barcodes-manual.csv", package = "hyperdags")
 mt.raw = read.csv(datafile)
 L = ncol(mt.raw)-1
@@ -249,11 +253,11 @@ for(i in 1:length(mt.orgs)) {
 margin.shift = 25
 mt.labs$Species = sapply(mt.labs$Species, transform_name)
 mt.plot.new = plot_stage_gen(expt.out[[6]]$best.graph,
-               v.labels=mt.labs,
-               label.style="points",
-               label.size = 3.5,
-               edge.alpha = 0.3,
-               point.size = 0.) +
+                             v.labels=mt.labs,
+                             label.style="points",
+                             label.size = 3.5,
+                             edge.alpha = 0.3,
+                             point.size = 0.) +
   coord_cartesian(clip = "off") +
   theme(plot.margin = margin(margin.shift, margin.shift, 0,0))
 
@@ -270,11 +274,11 @@ for(i in 1:length(pt.orgs)) {
 }
 pt.labs$Species = sapply(pt.labs$Species, transform_name)
 pt.plot.new = plot_stage_gen(expt.out[[7]]$best.graph,
-               v.labels=pt.labs,
-               label.style="points",
-               label.size = 3.5,
-               edge.alpha = 0.3,
-               point.size = 0.) +
+                             v.labels=pt.labs,
+                             label.style="points",
+                             label.size = 3.5,
+                             edge.alpha = 0.3,
+                             point.size = 0.) +
   coord_cartesian(clip = "off") +
   theme(plot.margin = margin(margin.shift, margin.shift, 0,0))
 
@@ -321,10 +325,10 @@ for(oDNA.expt in 1:3) {
     genes = colnames(pt.raw)[2:(L+1)]
   } else if(oDNA == "AML") {
     genes = c("FLT3", "NPM1", "WT1", "DNMT3A", "KRAS", "NRAS", "RUNX1",
-                       "IDH1", "IDH2", "PTPN11", "SRSF2", "ASXL1", "BCOR", "STAG2",
-                       "TP53", "U2AF1", "SF3B1", "TET2", "CSF3R", "JAK2", "GATA2",
-                       "EZH2", "PPM1D", "SETBP1", "KIT", "CBL", "PHF6", "MYC", "ETV6",
-                       "MPL", "SMC3")
+              "IDH1", "IDH2", "PTPN11", "SRSF2", "ASXL1", "BCOR", "STAG2",
+              "TP53", "U2AF1", "SF3B1", "TET2", "CSF3R", "JAK2", "GATA2",
+              "EZH2", "PPM1D", "SETBP1", "KIT", "CBL", "PHF6", "MYC", "ETV6",
+              "MPL", "SMC3")
   }
 
   # populate a matrix that will record features present when each feature is gained, and vice versa
@@ -352,16 +356,16 @@ for(oDNA.expt in 1:3) {
     labs(x="Other losses preceding loss", y="Other losses following loss")
 
   oDNA.g2[[oDNA.expt]] = as.ggplot(pheatmap(mat.dep,
-                                  treeheight_row = 0, treeheight_col = 0,
-                                  fontsize_row = 6, fontsize_col = 6))
+                                            treeheight_row = 0, treeheight_col = 0,
+                                            fontsize_row = 6, fontsize_col = 6))
 }
 
 png("oDNA-both-new.png", width=800*sf, height=600*sf, res=72*sf)
 ggarrange(mt.plot.new, pt.plot.new,
           oDNA.g[[1]] + theme_minimal(),
-         oDNA.g[[2]] + theme_minimal(),
-        nrow = 2, ncol=2, labels=c("A", "B", "C", "D")
-          )
+          oDNA.g[[2]] + theme_minimal(),
+          nrow = 2, ncol=2, labels=c("A", "B", "C", "D")
+)
 dev.off()
 
 png("oDNA-points.png", width=800*sf, height=400*sf, res=72*sf)
